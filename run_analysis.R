4083b3b28d6e04bd7a19ef1adb4715b76007f856
GeorgiosVrentzos <gvrentzos@yahoo.com>
2015-07-24 18:51:49 +0100

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,10 +1,10 @@
 # Step1. Merges the training and the test sets to create one data set.
-trainData <- read.table("/Users/georgevrentzos/Documents/R/CleaningData_Assign/UCI HAR Dataset/train/X_train.txt")
-trainLabel <- read.table("/Users/georgevrentzos/Documents/R/CleaningData_Assign/UCI HAR Dataset/train/y_train.txt")
-trainSubject <- read.table("/Users/georgevrentzos/Documents/R/CleaningData_Assign/UCI HAR Dataset/train/subject_train.txt")
-testData <- read.table("/Users/georgevrentzos/Documents/R/CleaningData_Assign/UCI HAR Dataset/test/X_test.txt")
-testLabel <- read.table("/Users/georgevrentzos/Documents/R/CleaningData_Assign/UCI HAR Dataset/test/y_test.txt") 
-testSubject <- read.table("/Users/georgevrentzos/Documents/R/CleaningData_Assign/UCI HAR Dataset/test/subject_test.txt")
+trainData <- read.table("./UCI HAR Dataset/train/X_train.txt")
+trainLabel <- read.table("./UCI HAR Dataset/train/y_train.txt")
+trainSubject <- read.table("./UCI HAR Dataset/train/subject_train.txt")
+testData <- read.table("./UCI HAR Dataset/test/X_test.txt")
+testLabel <- read.table("./UCI HAR Dataset/test/y_test.txt") 
+testSubject <- read.table("./UCI HAR Dataset/test/subject_test.txt")
 MergeData <- rbind(trainData, testData)
 MergeLabel <- rbind(trainLabel, testLabel)
 MergeSubject <- rbind(trainSubject, testSubject)
@@ -12,7 +12,7 @@ MergeSubject <- rbind(trainSubject, testSubject)
 
 # Step2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 
-features <- read.table("/Users/georgevrentzos/Documents/R/CleaningData_Assign/UCI HAR Dataset/features.txt")
+features <- read.table("./UCI HAR Dataset/features.txt")
 meanStdIndices <- grep("mean\\(\\)|std\\(\\)", features[, 2])
 MergeData <- MergeData[, meanStdIndices]
 names(MergeData) <- gsub("\\(\\)", "", features[meanStdIndices, 2]) # remove "()"
@@ -22,7 +22,7 @@ names(MergeData) <- gsub("-", "", names(MergeData)) # remove "-" in column names
 
 # Step3. Uses descriptive activity names to name the activities in the data set
 
-activity <- read.table("/Users/georgevrentzos/Documents/R/CleaningData_Assign/UCI HAR Dataset//activity_labels.txt")
+activity <- read.table("./UCI HAR Dataset//activity_labels.txt")
 activity[, 2] <- tolower(gsub("_", "", activity[, 2]))
 substr(activity[2, 2], 8, 8) <- toupper(substr(activity[2, 2], 8, 8))
 substr(activity[3, 2], 8, 8) <- toupper(substr(activity[3, 2], 8, 8))
